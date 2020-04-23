16a0137f6c3ac0315e7d87c7deb698267c4c52bd
sid-137 <sid-137@users.noreply.github.com>
2020-04-23 20:34:34 +0530

    add Pattern block

---
+++ Pattern.v
@@ -0,0 +1,79 @@
+`timescale 1ns / 1ps
+
+//Pattern Block
+module Pattern #(parameter NX = 6, NH = 30, BITS = 16) (
+	input						clk,		//Clock
+	input						TR,			//Training Signal (from Control)		
+	input						VL,			//Validation Signal (from Control)
+	input						SW,			//Store Weights signal (from Control)
+	input	[NX:0][BITS-1:0]	W1,			//Weights - hidden layer (from Architecture)
+	input	[NH:0][BITS-1:0]	W2,			//Weights - output layer (from Architecture)
+	output	[BITS-1:0]			lr,			//-Learning Rate (to Architecture)
+	output	[NX-1:0][BITS-1:0]	x,			//Neural Network input (to Architecture)
+	output	[BITS-1:0]			y,			//Output label (to Architecture)
+	output	[BITS-1:0]			TRAIN,		//# Training samples (to Control)
+	output	[BITS-1:0]			VALID,		//# Validation samples (to Control)
+	output	[BITS-1:0]			EPOCH		//# Epochs (to Control)
+);
+
+parameter N_train = 7;		// #Train
+parameter N_validate = 3;	// #Valid
+parameter N_Epochs = 100;	// #Epoch
+
+assign TRAIN = N_train;
+assign VALID = N_validate;
+assign EPOCH = N_Epochs;
+
+// Training & Validation data
+reg [NX-1:0][BITS-1:0]	X_train	[N_train-1:0];
+reg 		[BITS-1:0]	Y_train	[N_train-1:0];
+reg [NX-1:0][BITS-1:0]	X_val 	[N_validate-1:0];
+reg 		[BITS-1:0] 	Y_val	[N_validate-1:0];
+
+// NN Weights
+reg	[NX:0][BITS-1:0]	WH;
+reg	[NH:0][BITS-1:0]	WO;
+
+reg [NX-1:0][BITS-1:0] dataX;	//NN input data
+reg [BITS-1:0] dataY;			//Output label
+reg [BITS-1:0] negLR;			//-learning rate
+reg [15:0] addr_t, addr_v;		//iterators
+
+initial begin
+	$readmemh("xdata.txt", X_train);
+	$readmemh("ydata.txt", Y_train);
+	$readmemh("xval.txt", X_val);
+	$readmemh("yval.txt", Y_val);
+	negLR <= 16'hFF_00;
+	addr_t <= 16'h0000;
+	addr_v <= 16'h0000;
+end
+
+always @ (posedge clk) begin
+	if (TR) begin					//Training
+		dataX <= X_train[addr_t];
+		dataY <= Y_train[addr_t];
+		addr_t <= addr_t + 1;
+		addr_v <= 0;
+	end
+
+	else if (VL) begin				//Validation
+		dataX <= X_val[addr_v];
+		dataY <= Y_val[addr_v];
+		addr_v <= addr_v + 1;
+		addr_t <= 0;
+	end
+
+	else if (SW) begin				//Store NN synaptic weights
+		WH <= W1;
+		WO <= W2;
+		// addr_v <= 0;
+		// addr_t <= 0;
+	end
+end
+
+assign x = dataX;
+assign y = dataY;
+assign learning_rate = negLR;
+
+endmodule // Pattern
---
+++ pattern_test.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+
+module Pattern_TB();
+
+parameter nx = 4;
+parameter nh = 6;
+parameter wordlen = 16;
+
+reg clk;
+reg TR, VL, SW;
+reg [nx:0][wordlen-1:0] W1;
+reg [nh:0][wordlen-1:0] W2;
+
+wire [wordlen-1:0] lr;
+wire [nx-1:0][wordlen-1:0] x;
+wire [wordlen-1:0] y;
+wire [wordlen-1:0] TRAIN, VALID, EPOCH;
+
+Pattern #(.NX(nx), .NH(nh), .BITS(wordlen)) PB(
+	.clk(clk),
+	.TR(TR),
+	.VL(VL),
+	.SW(SW),
+	.W1(W1),
+	.W2(W2),
+	.lr(lr),
+	.x(x),
+	.y(y),
+	.TRAIN(TRAIN),
+	.VALID(VALID),
+	.EPOCH(EPOCH)
+);
+
+initial begin
+	$dumpfile("pattern.vcd");
+	$dumpvars(0, Pattern_TB);
+	clk <= 1'b0;
+	TR <= 1'b0;
+	VL <= 1'b0;
+	SW <= 1'b0;
+end
+
+always #10 clk <= ~ clk;
+
+always begin
+	#10 TR <= 1'b1;
+	#20 TR <= 1'b0;
+	#40 TR <= 1'b1;
+	#20 TR <= 1'b0;
+	#40 TR <= 1'b1;
+	#20 TR <= 1'b0;
+	#40 TR <= 1'b1;
+	#20 TR <= 1'b0;
+	#60 VL <= 1'b1;
+	#20 VL <= 1'b0;
+	#60 VL <= 1'b1;
+	#20 VL <= 1'b0;
+	#60 VL <= 1'b1;
+	#20 VL <= 1'b0;
+	#40 VL <= 1'b1;
+	#20 VL <= 1'b0;
+	#40 TR <= 1'b1;
+	#20 TR <= 1'b0;
+	#20 VL <= 1'b1;
+	#20 VL <= 1'b0;
+	#30 $stop;
+end
+
+endmodule // Pattern_TB
